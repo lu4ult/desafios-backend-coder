c491c9f9604ed564986de044f27d76e9c093bfec
Lautaro <lu4ult@gmail.com>
2023-11-01 16:31:42 -0300

    agrega socket y /realtimeproducts

--- app.js
+++ app.js
@@ -23,79 +23,40 @@ app.use(express.static(__dirname + '/src/public'));
 
 
 
-// app.get('/', (req, res) => {
-//     // res.render(__dirname + '/src/views/index', {});
-//     res.render('index', {});
-// })
 
 app.get('/', async (req, res) => {
     const productos = await productManager.getProducts();
-    console.log(productos)
     // res.render(__dirname + '/src/views/index', {});
     res.render('home', { productos });
-})
+});
 
 app.get('/realtimeproducts', (req, res) => {
     // res.render(__dirname + '/src/views/index', {});
-    res.render('realTimeProducts', {});
-})
+    res.render('realTimeProducts');
+});
 
-app.get('/productos', async (req, res) => {
-    const { limit } = req.query;
 
-    if (limit === undefined) {
-        res.status(200).json(await productManager.getProducts());
-    }
-    else {
-        let productos = productManager.getProducts();
 
-        while (productos.length > parseInt(limit)) {
-            productos.pop();
+app.post('/', async (req, res) => {
+    const { title, description, code, price, status = true, stock, category, thumbnails = [] } = req.body;
+
+    try {
+        let response = await productManager.addProduct(title, description, code, price, status, stock, category, thumbnails);
+
+        if (response.error) {
+            res.status(400).json(response);
         }
 
-        res.status(200).json(productos);
+        //Acá la parte de socket:
+        let productosTodos = await productManager.getProducts();
+        socketServer.emit('productosTodos', productosTodos);
+
+        res.status(200).json(response);
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ error: error, message: 'Ups! Algo salió mal' });
     }
-});
-
-
-
-app.get('/productos/:pid', (req, res) => {
-    const { pid } = req.params;
-    const productIdDeseado = parseInt(pid);
-
-    let producto = productManager.getProductById(productIdDeseado);
-    if (producto) {
-        res.status(200).json(producto);
-    }
-    else {
-        res.status(404).send(`El producto con ID ${productIdDeseado} no existe.`);
-    }
-});
-
-// app.post('/productos', async (req, res) => {
-//     const { title, description, code, price, status = true, stock, category, thumbnails = [] } = req.body;
-
-//     try {
-//         let response = await productManager.addProduct(title, description, code, price, status, stock, category, thumbnails);
-
-//         if (response.error) {
-//             res.status(400).json(response);
-//         }
-//         res.status(200).json(response);
-
-//     } catch (error) {
-//         console.log(error)
-//         res.status(500).json({ error: error, message: 'Ups! Algo salió mal' });
-//     }
-// });
-
-app.post('/', (req, res) => {
-    const { msg } = req.body;
-
-    console.log(msg)
-    socketServer.emit('messageFromPost', msg);
-
-    res.status(200).send('msg enviado por socket');
 });
 
 
@@ -104,30 +65,9 @@ const httpServer = app.listen(PORT, () => { console.log("server iniciado") });
 const socketServer = new Server(httpServer);
 
 socketServer.on('connection', socket => {
-    // console.log("nuevo cliente socket!", socket.id)
-    // console.log(socket.clientCount)
-    socket.on('message', data => {
-        console.log(`${socket.id}: ${data}`)
-        // console.log("nuevo mensaje por socket")
-        console.log(data)
-    });
-
     socket.on('disconnect', () => {
-        console.log(`Usuario desconectado`)
+        console.log(`Usuario desconectado`);
     });
-
-    socket.emit('saludosDesdeElback', 'Bienvenidos a wesocket!');
-
-    socket.on('respuestaDesdeFront', data => {
-        console.log(`${socket.id}: ${data}`)
-    })
-
-    socket.on('newProduct', producto => {
-        console.log(producto)
-        productos.push(producto);
-
-        socket.emit('products', productos);
-    })
 });
 
 
--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,48 +1,13 @@
 const socket = io();
-console.log("holaaaa")
 
+// socket.emit('message', 'soy un mensajes desde el cliente');
 
-// const socket = io.connect('http://localhost:8080', { 'forceNew': true });
-
-socket.emit('message', 'soy un mensajes desde el cliente');
-
-socket.on('saludosDesdeElback', msg => {
-    console.log(msg)
-
-    socket.emit('respuestaDesdeFront', 'Muchas gracias!')
-});
-
-
-const form = document.getElementById("form");
-form.onsubmit = (e) => {
-    e.preventDefault();
-    const nombre = document.getElementById("name").value;
-    const price = document.getElementById("price").value;
-
-    console.log(price)
-
-
-
-    const producto = { nombre, price };
-    console.log(producto)
-
-    socket.emit('newProduct', producto);
-}
-
-socket.on('products', (products) => {
-    console.log(products)
-
+socket.on('productosTodos', (productosTodos) => {
     let contentHtml = '';
-    products.forEach(pr => {
+    productosTodos.forEach(pr => {
         console.log(pr)
-        contentHtml += `<p>${pr.price}</p>`;
+        contentHtml += `<li>${pr.title}, ${pr.price} $ ${pr.stock}</li>`;
     })
 
     document.getElementById("products").innerHTML = contentHtml;
-    // console.log(contentHtml)
-})
-
-
-socket.on('messageFromPost', (msg) => {
-    console.log(msg);
-})
+});
